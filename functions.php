c4cb9fb288e06d6abc3c4df437901e33d049cc53
Mitchell Bray <mitchellbray@CPE-58-168-25-38.lns5.cht.bigpond.net.au>
2015-02-10 10:14:10 +1100

    cleanup for new core

--- functions.php
+++ functions.php
@@ -1,7 +1,26 @@
 <?php
-function scripts() {
-wp_enqueue_script('jquery'); 
-wp_enqueue_style ( 'main', get_stylesheet_directory_uri() . '/assets/css/site.css', false);
-wp_enqueue_script( 'site', get_stylesheet_directory_uri() . '/assets/js/min/site-min.js');
+add_action( 'wp_loaded', 'parent_prefix_load_classes', 10 );
+
+function parent_prefix_load_classes()
+{
+
+	$vars = array();
+	$vars['background'] = tr_option_field("[background]");
+	$vars['dark'] = tr_option_field("[dark]");
+	$vars['light'] = tr_option_field("[light]");
+	$vars['primary'] = tr_option_field("[primary]");
+	$vars['secondary'] = tr_option_field("[secondary]");
+	
+	$vars['base_font_size'] = tr_option_field("[base_font_size]");
+	$vars['small_heading'] = tr_option_field("[small_heading]");
+	$vars['medium_heading'] = tr_option_field("[medium_heading]");
+	$vars['large_heading'] = tr_option_field("[large_heading]");
+	$vars['huge_heading'] = tr_option_field("[huge_heading]");
+
+	$vars = array_filter($vars, 'strlen'); // removes unset array values
+
+	
+	$css = csscrush_file(__DIR__.'/assets/css/site.css',array('vars'=>$vars, 'minify'=>false));
+	wp_enqueue_style ( 'site', $css, false);
+
 }
-add_action('wp_enqueue_scripts', 'scripts', 100);
